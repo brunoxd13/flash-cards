509ec5d346b3e5444e8359020e84e42ebbacf521
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2019-02-07 23:53:06 -0200

    chore: add redux to app

---
+++ store/actions/categories.js
@@ -0,0 +1,16 @@
+export const RECIVE_CATEGORIES = "RECIVE_CATEGORIES";
+
+export function reciveCategories(categories) {
+  return {
+    type: RECIVE_CATEGORIES,
+    categories
+  };
+}
+
+export const fetchCategories = () => {
+  return dispatch => {
+    // return Api.getCategories().then(categories =>
+    //   dispatch(reciveCategories(categories))
+    // );
+  };
+};
