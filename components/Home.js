4c6e5eceeb5c927b87bce0e3c5ac6624f99b68d0
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2019-02-09 23:16:28 -0200

    style: Header bar style

--- components/Home.js
+++ components/Home.js
@@ -2,15 +2,20 @@ import React from "react";
 import { Container } from "native-base";
 import SimpleCardContainer from "./SimpleCardContainer";
 import { Button } from "react-native";
+import AddCategory from "./AddCategory";
 
 export default class Home extends React.Component {
   static navigationOptions = {
     headerTitle: "Flash Cards",
+    headerTintColor: "#FFFFFF",
+    headerStyle: {
+      backgroundColor: "#5EC9F7"
+    },
     headerRight: (
       <Button
         onPress={() => alert("This is a button!")}
         title="+"
-        color="#87CEFA"
+        color="#FFFFFF"
       />
     )
   };
@@ -18,6 +23,7 @@ export default class Home extends React.Component {
   render() {
     return (
       <Container>
+        {/* <AddCategory /> */}
         <SimpleCardContainer />
       </Container>
     );
