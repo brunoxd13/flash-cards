b12a109bdd8d6b9d2466a4c6a0ed4bff4576a4e0
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2019-02-10 22:30:07 -0200

    feat: created simple card component

---
+++ components/base/SimpleCard.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default class CardDetail extends Component {
+  render() {
+    return (
+      <View style={styles.card}>
+        <Text style={styles.strong}>Title</Text>
+        <Text style={styles.ligth}>5 Questions</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  card: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 10,
+    padding: 10,
+    margin: 3,
+    color: "#222222"
+  },
+  strong: {
+    fontWeight: "bold"
+  },
+  ligth: {
+    color: "#878787"
+  }
+});
