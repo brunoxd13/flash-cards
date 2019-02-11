057c63ef340561ef9634cb951bc371c0b5053d43
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2019-02-10 22:29:25 -0200

    feat: add add deck route

--- components/MainNavigator.js
+++ components/MainNavigator.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { createAppContainer, createStackNavigator } from "react-navigation";
 import Home from "./Home";
+import AddDeck from "./AddDeck";
 
 const MainNavigator = createStackNavigator({
   Home: {
     screen: Home
+  },
+  NewDeck: {
+    screen: AddDeck
   }
 });
 
