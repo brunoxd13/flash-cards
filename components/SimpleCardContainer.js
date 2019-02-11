ed7358191c47da5764af2dd0d1ce8631e1602b5f
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2019-02-11 21:49:13 -0200

    chore: removed native base from container

--- components/SimpleCardContainer.js
+++ components/SimpleCardContainer.js
@@ -1,31 +1,28 @@
 import React from "react";
 import { ScrollView } from "react-native";
 import SimpleCard from "./base/SimpleCard";
-import { Content } from "native-base";
 
 const SimpleCardContainer = ({ categories }) => {
   return (
     <ScrollView>
-      <Content padder>
-        <SimpleCard />
-        <SimpleCard />
-        <SimpleCard />
-        <SimpleCard />
-        <SimpleCard />
-        <SimpleCard />
-        <SimpleCard />
-        <SimpleCard />
-        <SimpleCard />
-        <SimpleCard />
-        <SimpleCard />
-        <SimpleCard />
-        <SimpleCard />
-        <SimpleCard />
-        <SimpleCard />
-        <SimpleCard />
-        <SimpleCard />
-        <SimpleCard />
-      </Content>
+      <SimpleCard />
+      <SimpleCard />
+      <SimpleCard />
+      <SimpleCard />
+      <SimpleCard />
+      <SimpleCard />
+      <SimpleCard />
+      <SimpleCard />
+      <SimpleCard />
+      <SimpleCard />
+      <SimpleCard />
+      <SimpleCard />
+      <SimpleCard />
+      <SimpleCard />
+      <SimpleCard />
+      <SimpleCard />
+      <SimpleCard />
+      <SimpleCard />
     </ScrollView>
   );
 };
