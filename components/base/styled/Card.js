37a1db8e6801f7d4c0925863319887bc2622f94e
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2019-02-12 23:09:38 -0200

    feat: Criado card styled

---
+++ components/base/styled/Card.js
@@ -0,0 +1,10 @@
+import styled from "styled-components/native";
+
+export const Card = styled.View`
+  flex: 1;
+  background-color: #ffffff;
+  margin: 3px;
+  border-radius: 10px;
+  color: #222222;
+  padding: ${props => `${props.padding || 10}px`};
+`;
