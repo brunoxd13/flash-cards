fbfbe3d9ff3b2213ab63b6e8459cbf589c861c0b
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2019-02-12 23:10:30 -0200

    reafactor: Ajusted to use styled components

--- components/base/CardDetail.js
+++ components/base/CardDetail.js
@@ -1,46 +1,36 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import HorizontalLine from "./HorizontalLine";
+import { Card } from "./styled/Card";
+import { StrongText, LigthText } from "./styled/Text";
+import { HorizontalLine } from "./styled/HorizontalLine";
 
 export default class CardDetail extends Component {
   render() {
     return (
-      <View style={styles.card}>
+      <Card padding={35}>
         <View style={styles.header}>
-          <Text style={styles.strong}>Title</Text>
-          <Text style={styles.ligth}>5 Questions</Text>
+          <StrongText>Title</StrongText>
+          <LigthText>5 Questions</LigthText>
         </View>
 
         <HorizontalLine />
+
         <View style={styles.header}>
-          <Text style={styles.strong}>History</Text>
+          <StrongText>History</StrongText>
         </View>
 
         <View style={styles.history}>
           <Text>Bruno</Text>
           <Text>75%</Text>
         </View>
-      </View>
+      </Card>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  card: {
-    flex: 1,
-    backgroundColor: "#FFFFFF",
-    borderRadius: 10,
-    padding: 35,
-    color: "#222222"
-  },
   header: {
     alignItems: "center"
-  },
-  strong: {
-    fontWeight: "bold"
-  },
-  ligth: {
-    color: "#878787"
   },
   history: {
     flex: 1,
--- components/base/SimpleCard.js
+++ components/base/SimpleCard.js
@@ -1,30 +1,14 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { Card } from "./styled/Card";
+import { StrongText, LigthText } from "./styled/Text";
 
 export default class SimpleCard extends Component {
   render() {
     return (
-      <View style={styles.card}>
-        <Text style={styles.strong}>Title</Text>
-        <Text style={styles.ligth}>5 Questions</Text>
-      </View>
+      <Card>
+        <StrongText>Title</StrongText>
+        <LigthText>5 Questions</LigthText>
+      </Card>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  card: {
-    flex: 1,
-    backgroundColor: "#FFFFFF",
-    borderRadius: 10,
-    padding: 10,
-    margin: 3,
-    color: "#222222"
-  },
-  strong: {
-    fontWeight: "bold"
-  },
-  ligth: {
-    color: "#878787"
-  }
-});
