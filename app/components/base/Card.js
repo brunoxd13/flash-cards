f6b78227ed3fcf209fd9b12bae4488781f825fc8
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2019-02-17 22:34:08 -0300

    feat: created card clikable

--- app/components/base/Card.js
+++ app/components/base/Card.js
@@ -8,3 +8,12 @@ export const Card = styled.View`
   color: #222222;
   padding: ${props => `${props.padding || 10}px`};
 `;
+
+export const CardClickable = styled.TouchableOpacity`
+  flex: 1;
+  background-color: #ffffff;
+  margin: 3px;
+  border-radius: 10px;
+  color: #222222;
+  padding: ${props => `${props.padding || 10}px`};
+`;
