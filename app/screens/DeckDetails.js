0b1148fa3a70f0d25730fd040f0faedfeaf9ee66
Bruno Russi Lautenschlager <bruno.lautenschlager@ciss.com.br>
2019-02-19 18:06:21 -0300

    refactor: DeckDetail final version

--- app/screens/DeckDetails.js
+++ app/screens/DeckDetails.js
@@ -1,11 +1,34 @@
 import React from "react";
+import { View, ScrollView } from "react-native";
 import { withNavigation } from "react-navigation";
+import { connect } from "react-redux";
 import CardDetail from "../components/base/CardDetail";
+import Button from "../components/base/Button";
 
-const DeckDetails = ({ navigation }) => {
-  const { deck } = navigation.state.params;
+const DeckDetails = ({ navigation, deck }) => {
+  return (
+    <ScrollView style={{ padding: 3, backgroundColor: "#E5E5E5" }}>
+      <CardDetail deck={deck} />
+      <View style={{ flex: 1, flexDirection: "row" }}>
+        <Button
+          style={{ flex: 1 }}
+          onPress={() => navigation.navigate("AddQuestion", { deck: deck })}
+        >
+          Add Card
+        </Button>
 
-  return <CardDetail deck={deck} />;
+        <Button primary style={{ flex: 1 }}>
+          Start Quiz
+        </Button>
+      </View>
+    </ScrollView>
+  );
 };
 
-export default withNavigation(DeckDetails);
+function mapStateToProps({ decks }, { navigation }) {
+  return {
+    deck: decks[navigation.state.params.deckTitle]
+  };
+}
+
+export default connect(mapStateToProps)(withNavigation(DeckDetails));
