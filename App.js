1297f22862473036677e44407d13b5a5a9169359
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2019-02-19 22:16:28 -0300

    feat: Notification to study

--- App.js
+++ App.js
@@ -2,8 +2,13 @@ import React, { Component } from "react";
 import MainNavigator from "./app/routes/MainNavigator";
 import { Provider } from "react-redux";
 import { Store } from "./app/store/index";
+import { setLocalNotification } from "./app/utils/Notification";
 
 export default class App extends Component {
+  componentDidMount() {
+    setLocalNotification();
+  }
+
   render() {
     return (
       <Provider store={Store}>
---
+++ app/utils/Notification.js
@@ -0,0 +1,49 @@
+import { Notifications, Permissions } from "expo";
+import { AsyncStorage } from "react-native";
+
+const NOTIFICATION_KEY = "FlashCards:notifications";
+
+function createNotification() {
+  return {
+    title: "Study Now!",
+    body: "Stay tuned to study 👋",
+    ios: {
+      sound: true
+    },
+    android: {
+      sound: true,
+      priority: "high",
+      sticky: false,
+      vibrate: true
+    }
+  };
+}
+
+export function setLocalNotification() {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then(data => {
+      if (data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS).then(({ status }) => {
+          if (status === "granted") {
+            Notifications.cancelAllScheduledNotificationsAsync();
+
+            let tomorrow = new Date();
+            tomorrow.setDate(tomorrow.getDate() + 1);
+            tomorrow.setHours(8);
+            tomorrow.setMintutes(15);
+
+            Notifications.scheduleLocalNotificationsAsync(
+              createNotification(),
+              {
+                time: tomorrow,
+                repeat: "day"
+              }
+            );
+
+            AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true));
+          }
+        });
+      }
+    });
+}
