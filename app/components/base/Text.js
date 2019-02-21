4f24ed1c92d129262369a953fb99c20f8976cb84
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2019-02-21 00:10:39 -0300

    feat: add font size and align text to Text components

--- app/components/base/Text.js
+++ app/components/base/Text.js
@@ -2,8 +2,12 @@ import styled from "styled-components/native";
 
 export const StrongText = styled.Text`
   font-weight: bold;
+  font-size: ${props => `${props.size || 16}px`};
+  ${({ center }) => center && `text-align: center;`}
 `;
 
 export const LigthText = styled.Text`
   color: #878787;
+  font-size: ${props => `${props.size || 16}px`};
+  ${({ center }) => center && `text-align: center;`}
 `;
